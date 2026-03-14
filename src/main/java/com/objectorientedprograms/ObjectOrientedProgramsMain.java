4546fcd85bb74ceb74b4171f79b2205cbd7d75e2
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 14:46:32 +0530

    UC5: Extend the above program to create a Player Object having Deck of

--- src/main/java/com/objectorientedprograms/ObjectOrientedProgramsMain.java
+++ src/main/java/com/objectorientedprograms/ObjectOrientedProgramsMain.java
@@ -3,7 +3,7 @@ package com.objectorientedprograms;
 import java.util.Scanner;
 
 /**
- * UC4 - Write a Program DeckOfCards.java, to initialize deck of cards having suit
+ * UC5 - Extend the above program to create a Player Object having Deck of Cards, and
  *
  * Requirements:
      *   (See PDF)
@@ -16,10 +16,10 @@ public class ObjectOrientedProgramsMain {
     public static void main(String[] args) {
         System.out.println("Welcome to ObjectOrientedPrograms");
         
-        System.out.println("\n=== Problem 4: Write a Program DeckOfCards.java, to initialize de ===");
+        System.out.println("\n=== Problem 5: Extend the above program to create a Player Object ===");
         // TODO: Implement problem solution
         // 
 
-        System.out.println("Problem 4 implemented!");
+        System.out.println("Problem 5 implemented!");
     }
 }
