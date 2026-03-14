a1fec647f01a8a50c811cf12519ecd9c7bdf4815
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 14:46:16 +0530

    UC2: Modify class Account to provide a method called debit that withdr

--- src/main/java/com/objectorientedprograms/ObjectOrientedProgramsMain.java
+++ src/main/java/com/objectorientedprograms/ObjectOrientedProgramsMain.java
@@ -3,7 +3,7 @@ package com.objectorientedprograms;
 import java.util.Scanner;
 
 /**
- * UC1 - Stock Account Management
+ * UC2 - Modify class Account to provide a method called debit that withdraws money
  *
  * Requirements:
      *   (See PDF)
@@ -16,10 +16,12 @@ public class ObjectOrientedProgramsMain {
     public static void main(String[] args) {
         System.out.println("Welcome to ObjectOrientedPrograms");
         
-        System.out.println("\n=== Problem 1: Stock Account Management ===");
-        // TODO: Implement problem solution
-        // 
-
-        System.out.println("Problem 1 implemented!");
+        System.out.println("\n=== UC2: Modify class Account to provide a method called de ===");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter name to edit: ");
+        String name = scanner.nextLine();
+        // TODO: Implement edit logic
+        System.out.println("Edit operation for: " + name);
+        scanner.close();
     }
 }
