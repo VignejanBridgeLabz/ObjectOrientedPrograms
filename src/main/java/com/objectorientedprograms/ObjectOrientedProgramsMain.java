32ac3db61f1c95e5943e8976104b75f5d09fecd5
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 14:46:05 +0530

    START: Display welcome message in ObjectOrientedProgramsMain

---
+++ src/main/java/com/objectorientedprograms/ObjectOrientedProgramsMain.java
@@ -0,0 +1,7 @@
+package com.objectorientedprograms;
+
+public class ObjectOrientedProgramsMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to ObjectOrientedPrograms");
+    }
+}
