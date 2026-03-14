a43c331d412b6fd7c75a1d8fe4af4e29204187f3
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 14:46:11 +0530

    UC1: Stock Account Management

--- src/main/java/com/objectorientedprograms/ObjectOrientedProgramsMain.java
+++ src/main/java/com/objectorientedprograms/ObjectOrientedProgramsMain.java
@@ -1,7 +1,25 @@
 package com.objectorientedprograms;
 
+import java.util.Scanner;
+
+/**
+ * UC1 - Stock Account Management
+ *
+ * Requirements:
+     *   (See PDF)
+ *
+ * @author  VignejanBridgeLabz
+ * @version 1.0
+ */
 public class ObjectOrientedProgramsMain {
+
     public static void main(String[] args) {
         System.out.println("Welcome to ObjectOrientedPrograms");
+        
+        System.out.println("\n=== Problem 1: Stock Account Management ===");
+        // TODO: Implement problem solution
+        // 
+
+        System.out.println("Problem 1 implemented!");
     }
 }
